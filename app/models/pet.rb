7e1ec11b713a509f217ae7f379aa988be4b9f05d
DanCema <dan.cemachovic@edhec.com>
2020-02-17 18:50:03 +0100

    Pet controller methods + views

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :race, presence: true
+  validates :age, presence: true
+  validates :description, presence: true
 end
