f92627dde400e7bcac92442b82a7049fc54a26fd
sybileduarte <sybile.duarte@edhec.com>
2020-02-17 17:38:35 +0100

    corrected bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   devise_for :users
-  root to: 'pets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pets do
     resources :bookings, only: [:new]
