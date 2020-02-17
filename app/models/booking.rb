4591ec0457746e33ecd4e98ca8a21f681eeaca12
sybileduarte <sybile.duarte@edhec.com>
2020-02-17 18:47:08 +0100

    Created Booking new, create and index + booking validates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
   has_many :reviews
+  validates :start_date, presence: true, uniqueness: true
+  validates :end_date, presence: true, uniqueness: true
 end
