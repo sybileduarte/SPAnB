7831c615d8c255808c37a4099465553875e4d641
DanCema <dan.cemachovic@edhec.com>
2020-02-19 10:29:21 +0100

    dc-js-creation-form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,24 +1,5 @@
 import "bootstrap";
 
+import { selectSpecie } from '../components/form_specie_selection';
 
-const chat = document.getElementById('chat')
-const inputChat = document.getElementById('input-chat')
-
-chat.addEventListener("click", (event) => {
-  inputChat.classList.toggle("d-none")
-  if (!inputChien.classList.contains("d-none")) {
-    inputChien.classList.add("d-none")
-  }
-})
-
-
-
-const chien = document.getElementById('chien')
-const inputChien = document.getElementById('input-chien')
-
-chien.addEventListener("click", (event) => {
-  inputChien.classList.toggle("d-none")
-  if (!inputChat.classList.contains("d-none")) {
-    inputChat.classList.add("d-none")
-  }
-})
+selectSpecie();
