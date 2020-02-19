c198e85a90d682f22dca3602532b06494abbc180
DanCema <dan.cemachovic@edhec.com>
2020-02-19 18:04:01 +0100

    add price to form - controller - presence in model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -11,6 +11,7 @@ class Pet < ApplicationRecord
   validates :description, presence: true
   validates :photo, presence: true
   validates :address, presence: true
+  validates :price, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
