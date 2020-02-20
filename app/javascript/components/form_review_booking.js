ee7fc61059d457dcf798f49285f5bc591cda154f
DanCema <dan.cemachovic@edhec.com>
2020-02-20 16:20:37 +0100

    list bookings + reviews

--- app/javascript/components/form_review_booking.js
+++ app/javascript/components/form_review_booking.js
@@ -1,10 +1,12 @@
 const showFormReview = () => {
   const btnReviews = document.querySelectorAll(".add-review")
-  btnReviews.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
-      event.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.children[2].classList.toggle("d-none")
+  if (btnReviews){
+    btnReviews.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        event.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].classList.toggle("d-none")
+      })
     })
-  })
-}
+  };
+};
 
 export { showFormReview };
